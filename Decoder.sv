2cfdbf4e4d54e6137ca56de1ec2c8b03ad20ce5c
Andrew Neto <eu@andrewneto.com>
2018-12-16 22:57:21 -0200

    CMN, TEQ implementados

--- Decoder.sv
+++ Decoder.sv
@@ -24,7 +24,7 @@ module decoder (
                     4'b1101 :
                         //MOV REGISTER, IMMEDIATE
                         if (Funct[5])   controls = 12'b000011010000;
-                        //MOV REGISTER, REGISTER AND LSL
+                        //MOV REGISTER, REGISTER AND LSL|LSR|ASR|ROR
                         else            controls = 12'b000010010000;
 
                     4'b1010 :
@@ -33,14 +33,26 @@ module decoder (
                         //CMP REGISTER, REGISTER
                         else            controls = 12'b000000000001;
 
+                    4'b1011 :
+                        //CMN REGISTER, IMMEDIATE
+                        if (Funct[5])   controls = 12'b000001000001;
+                        //CMN REGISTER, REGISTER
+                        else            controls = 12'b000000000001;
+
                     4'b1000 :
                         //TST REGISTER, IMMEDIATE
                         if (Funct[5])   controls = 12'b000001000001;
                         //TST REGISTER, REGISTER
                         else            controls = 12'b000000000001;
 
+                    4'b1001 :
+                        //TEQ REGISTER, IMMEDIATE
+                        if (Funct[5])   controls = 12'b000001000001;
+                        //TEQ REGISTER, REGISTER
+                        else            controls = 12'b000000000001;
+
                     default:
-                        //ALU FUNCTIONS
+                        //ALU FUNCTIONS ADD|SUB|AND|ORR
                         if (Funct[5])   controls = 12'b000001010001;
                         else            controls = 12'b000000010001;
                 endcase
@@ -102,13 +114,16 @@ module decoder (
     always_comb
         if (ALUOp) begin                 // which DP Instr?
             case(Funct[4:1])
-                4'b0001: ALUControl = 3'b100; // EOR (XOR)
                 4'b0100: ALUControl = 3'b000; // ADD
                 4'b0010: ALUControl = 3'b001; // SUB
                 4'b0000: ALUControl = 3'b010; // AND
                 4'b1100: ALUControl = 3'b011; // ORR
-                4'b1000: ALUControl = 3'b010; // TST = AND SINALIZADO SEM ATUAILZAR REGISTRADOR
-                4'b1010: ALUControl = 3'b001; // CMP
+
+                4'b0001: ALUControl = 3'b100; // EOR (XOR)
+                4'b1000: ALUControl = 3'b010; // TST (AND)
+                4'b1001: ALUControl = 3'b100; // TEQ (XOR)
+                4'b1010: ALUControl = 3'b001; // CMP (ADD)
+                4'b1011: ALUControl = 3'b001; // CMN (SUB)
                 default: ALUControl = 3'bx;  // unimplemented
             endcase
             // update flags if S bit is set
@@ -127,7 +142,7 @@ endmodule
 
 
 //CMP ? compare
-//-Flags set to result of (Rn ? Operand2).
+//-Flags set to result of (Rn - Operand2).
 //CMN ? compare negative
 //-Flags set to result of (Rn + Operand2).
 //TST ? bitwise test
